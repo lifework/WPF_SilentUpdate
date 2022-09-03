c0c051da9a08d9dfa27c970c6caf73edc8599557
lifework <kobayashi@lifework.tech>
2022-09-03 21:51:45 +0900

    サイレントアップデート

--- WPF_SilentUpdate/App.xaml.cs
+++ WPF_SilentUpdate/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,10 @@ namespace WPF_SilentUpdate
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            RelaunchHelper.RegisterApplicationRestart();
+            StoreManager.CheckUpdateAndInstall();
+        }
     }
 }
---
+++ WPF_SilentUpdate/StoreManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Services.Store;
+
+namespace WPF_SilentUpdate
+{
+    class StoreManager
+    {
+        public static void CheckUpdateAndInstall()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Thread.Sleep(60 * 1000);
+                        await SilentDownloadAndInstallUpdatesAsync();
+                    }
+                    catch (Exception e)
+                    {
+                        break;
+                    }
+                }
+            });
+        }
+
+
+        public static async Task<bool> SilentDownloadAndInstallUpdatesAsync()
+        {
+
+            StoreContext context = StoreContext.GetDefault();
+            if (context.CanSilentlyDownloadStorePackageUpdates == false)
+            {
+                return false;
+            }
+
+            IReadOnlyList<StorePackageUpdate> storePackageUpdates = await context.GetAppAndOptionalStorePackageUpdatesAsync();
+            if (storePackageUpdates.Count == 0)
+            {
+                return false;
+            }
+
+            StorePackageUpdateResult installResult = await context.TrySilentDownloadAndInstallStorePackageUpdatesAsync(storePackageUpdates);
+            if (installResult.OverallState != StorePackageUpdateState.Completed)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
