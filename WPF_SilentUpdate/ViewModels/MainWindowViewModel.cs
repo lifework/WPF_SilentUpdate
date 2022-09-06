4340f13f0a34e4ebe6b6e58a2b39fe9d84660323
lifework <kobayashi@lifework.tech>
2022-09-06 17:43:55 +0900

    DelegateCommand に変更

--- WPF_SilentUpdate/ViewModels/MainWindowViewModel.cs
+++ WPF_SilentUpdate/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
@@ -13,10 +14,8 @@ namespace WPF_SilentUpdate.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        public MainWindowViewModel()
-        {
-            UpdateUptimeAsync();
-        }
+        public DelegateCommand RestartCommand { get; private set; }
+        public DelegateCommand UpdateCommand { get; private set; }
 
         public int ProcessId => Environment.ProcessId;
         public Assembly ExecutingAssembly => Assembly.GetExecutingAssembly();
@@ -24,6 +23,26 @@ namespace WPF_SilentUpdate.ViewModels
         public string AssemblyVersion => $"{ExecutingAssembly.GetName().Version?.ToString() ?? "Unknown"} / {PackageVersion}";
         public string PackageVersion => StoreManager.PackageVersion();
         public ReactivePropertySlim<int> UptimeSeconds { get; set; } = new(0);
+
+        public MainWindowViewModel()
+        {
+            RestartCommand = new DelegateCommand(OnRestartCommand);
+            UpdateCommand = new DelegateCommand(OnUpdateCommand);
+
+            UpdateUptimeAsync();
+        }
+
+        private async void OnRestartCommand()
+        {
+            int x = 1;
+            int y = 0;
+            int z = x / y;
+        }
+
+        private async void OnUpdateCommand()
+        {
+            await StoreManager.SilentDownloadAndInstallUpdatesAsync();
+        }
 
         public void UpdateUptimeAsync()
         {
